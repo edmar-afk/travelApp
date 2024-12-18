2915fef96a7cb4eac2d922b199d8622130d9970d
project2479 <jaywrsnp6@gmail.com>
2024-12-18 09:54:34 +0800

    new

--- src/routes/Map.jsx
+++ src/routes/Map.jsx
@@ -1,8 +1,7 @@
-import BottomNav from "../components/BottomNav";import map from "../assets/img/map.png";
-import "react-tooltip/dist/react-tooltip.css";
+import BottomNav from "../components/BottomNav";import map from "../assets/img/map.png";import "react-tooltip/dist/react-tooltip.css";
 import { Tooltip } from "react-tooltip";
 import { Link } from "react-router-dom"; // Import Link for navigation
-
+import bakaFalls from '../assets/img/resorts/baka-falls.jpg';
 function Map() {
 	return (
 		<>
@@ -16,25 +15,21 @@ function Map() {
 				</div>
 			</div>
 
-			<div>
-				<a
-					id="resort-link"
-					className="absolute z-50 top-1">
-					ss
-				</a>
+			<div className="absolute z-50 top-[175px] right-[2px]">
+				<a id="baka-falls" className=" border-2 p-2 rounded-full px-3 py-0.5"></a>
 				<Tooltip
-					anchorSelect="#resort-link" // Associates tooltip with this anchor
+					anchorSelect="#baka-falls" // Associates tooltip with this anchor
 					clickable // Allows interaction with the tooltip content
 				>
 					<div className="p-2 text-center">
 						<img
-							src="https://via.placeholder.com/150" // Replace with actual image URL
+							src={bakaFalls}
 							alt="Resort"
 							className="w-24 h-24 object-cover mx-auto mb-2"
 						/>
-						<p className="text-sm font-bold mb-1">Beautiful Resort</p>
+						<p className="text-sm font-bold mb-1">Baka Falls</p>
 						<Link
-							to="/resort/123" // Replace with the actual resort link
+							to="/visitorDashboard/buildingDetails/5" // Replace with the actual resort link
 							className="text-blue-500 underline text-sm">
 							View Resort Details
 						</Link>
