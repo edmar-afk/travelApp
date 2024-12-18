85e623c4da09756ab87f271164b23c55ce757cbc
project2479 <jaywrsnp6@gmail.com>
2024-12-19 07:50:23 +0800

    new

--- src/components/status/BuildingOngoing.jsx
+++ src/components/status/BuildingOngoing.jsx
@@ -1,4 +1,5 @@
-import api from "../../assets/api";import { useEffect, useState } from "react";
+import api from "../../assets/api";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import DomainIcon from "@mui/icons-material/Domain";
 import resortLogo from "../../assets/img/resortlogo.avif";
@@ -19,19 +20,19 @@ function BuildingApproved() {
 	return (
 		<>
 			<div className="">
-				<p className="my-4 font-bold p-4">Spots that Ongoing Process of Papers by the Tourism</p>
+				<p className="my-4 font-bold p-4">Spots Approved by the Tourism</p>
 				<div className="flex flex-row flex-wrap justify-evenly items-center">
 					{places.map((place) => (
 						<Link
 							key={place.id}
 							to={`/visitorDashboard/buildingDetails/${place.id}`}
-							className="flex flex-col relative mb-4 mx-4 w-20 h-20 ">
+							className="flex flex-col relative mb-8 mx-4 w-20 h-20">
 							<div className="absolute top-0 h-full w-full rounded-2xl"></div>
-							<div className="flex flex-col justify-center items-center">
+							<div className="flex flex-col justify-center items-center gap-3">
 								<img
-									src={resortLogo}
-									alt=""
-									className="-mb-4"
+									src={place.logo || resortLogo} // Fallback to resortLogo if place.logo is empty
+									alt={`${place.name || "Resort"} logo`}
+									className="-mb-4 w-14"
 								/>
 								<div className="text-center text-gray-800 text-xs">
 									<div className="flex items-center justify-center">
